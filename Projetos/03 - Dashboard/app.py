48389935df0c673a00c60750738dbaebed19e75b
Diego Neves <pdcassiano@gmail.com>
2024-05-21 22:24:17 -0300

    update: criando projeto 3

--- Projetos/03 - Dashboard/app.py
+++ Projetos/03 - Dashboard/app.py
@@ -13,38 +13,33 @@ st.title("Dashboard")
 container = st.container(border=True)
 
 
-
 with container:
     df = pd.read_excel("./data/vendas.xlsx")
     # st.dataframe(df)
     acumulado_vendas_por_cidade = df[['City', 'Total']].groupby('City').sum()
     media_vendas_por_cidade = df[[  'City', 'Total']].groupby('City').mean()
+    
+    quantidade_de_vendas = df.shape[0]
+
+    st.write(df)
+
+    col1, col2, col3 = st.columns(3)
+    col1.metric(label="Quantidade de vendas", value=quantidade_de_vendas)
+
+    st.write(quantidade_de_vendas)
 
     col1, col2 = st.columns(2)
-    
     with col1:
-        fig, ax = plt.subplots()
-        bars = ax.bar(
-            acumulado_vendas_por_cidade.index, 
-            acumulado_vendas_por_cidade['Total'])
-        ax.set_title('Acumulado de vendas por Cidade')
-        ax.set_ylabel('Acumulado de vendas')
-        st.pyplot(fig)
-
-        for bar in bars:
-            height = bar.get_height()
-            ax.text(
-                bar.get_x() + bar.get_width() / 2.0,
-                height,
-                f'{height:.0f}',
-                ha='center', va='bottom'
-            )
-
+        st.scatter_chart(acumulado_vendas_por_cidade)
 
 
     with col2:
-        fig, ax = plt.subplots()
-        ax.barh(media_vendas_por_cidade.index, acumulado_vendas_por_cidade['Total'])
-        ax.set_xlabel('Média de vendas')
-        ax.set_title('Média de vendas por Cidade')
-        st.pyplot(fig)
+        st.area_chart(media_vendas_por_cidade)
+
+
+    preco_por_tipo_de_pagamento = df[['Payment', 'Total']].groupby('Payment').sum()
+    st.bar_chart(preco_por_tipo_de_pagamento)
+
+    quantidade_vendidas_por_genero_e_tipo = df[['Gender', 'Customer type', 'Quantity']].groupby(['Gender', 'Customer type']).sum()
+    st.write(quantidade_vendidas_por_genero_e_tipo)
+    # st.bar_chart(quantidade_vendidas_por_genero_e_tipo, x='Customer type', y='Quantity')
