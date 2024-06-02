7b35d1079f246986e14fc33d3e40a55acd40a725
Diego Neves <pdcassiano@gmail.com>
2024-06-02 13:35:08 -0300

    feat: adicionado títulos aos gráficos

--- Projetos/02 - Galeria de Fotos/pages/enviar_imagens.py
+++ Projetos/02 - Galeria de Fotos/pages/enviar_imagens.py
@@ -33,9 +33,3 @@ if enviar:
         df_imagem.to_csv(os.path.join('data', 'imagens.csv'), index=False)
         if imagem:
             gravar_imagem(imagem)
-
-
-
-# UploadedFile(file_id='f5ac55e1-1f95-4b1a-954f-e2f5de6ea59b', name='logo.png', type='image/png', size=3944, _file_urls=file_id: "f5ac55e1-1f95-4b1a-954f-e2f5de6ea59b"
-# upload_url: "/_stcore/upload_file/55e630bd-067d-434d-919a-08cf221fff66/f5ac55e1-1f95-4b1a-954f-e2f5de6ea59b"
-# delete_url: "/_stcore/upload_file/55e630bd-067d-434d-919a-08cf221fff66/f5ac55e1-1f95-4b1a-954f-e2f5de6ea59b")
--- Projetos/03 - Dashboard/app.py
+++ Projetos/03 - Dashboard/app.py
@@ -18,7 +18,7 @@ with container:
     media_vendas_por_cidade = df[[  'City', 'Total']].groupby('City').mean()
     
     quantidade_de_vendas = df.shape[0]
-    media_de_vendas = df['Total'].mean()
+    media_de_vendas = round(df['Total'].mean(), 2)
     quantidade_vendida = df['Quantity'].sum()
 
     # st.write(df.describe())
@@ -33,7 +33,7 @@ with container:
     with col2:
         content = st.container(border=True)
         with content:
-            st.metric(label="Média de vendas", value=media_de_vendas)
+            st.metric(label="Média de vendas", value="R$ " + str(media_de_vendas))
     
     with col3:
         content = st.container(border=True)
@@ -42,14 +42,17 @@ with container:
 
     col1, col2 = st.columns(2)
     with col1:
+        st.subheader("Acumulado de vendas por Cidade")
         st.scatter_chart(acumulado_vendas_por_cidade)
 
 
     with col2:
+        st.subheader("Média de vendas por Cidade")
         st.area_chart(media_vendas_por_cidade)
 
 
     preco_por_tipo_de_pagamento = df[['Payment', 'Total']].groupby('Payment').sum()
+    st.subheader("Vendas por tipo de pagamento")
     st.bar_chart(preco_por_tipo_de_pagamento)
 
     quantidade_vendidas_por_genero_e_tipo = df[['Gender', 'Customer type', 'Quantity']].groupby(['Gender', 'Customer type']).sum()
