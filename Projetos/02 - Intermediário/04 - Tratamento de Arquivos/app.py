ec1e085f3de2bf450a976bce672990bcc42822ea
Diego Neves <pdcassiano@gmail.com>
2024-06-09 09:18:24 -0300

    fix: projeto 04 - retirado sidebar, focar no objetivo do projeto

--- Projetos/02 - Intermediário/04 - Tratamento de Arquivos/app.py
+++ Projetos/02 - Intermediário/04 - Tratamento de Arquivos/app.py
@@ -6,21 +6,6 @@ st.set_page_config(
     page_icon="📁",
     layout="wide")
 
-df = ""
-file = ""
-
-def receberArquivo(arq):
-    global file
-    file = arq
-
-def criarDataFrame(arq):
-    global df
-    df = pd.read_excel(arq)
-
-def getDataFrame():
-    global df
-    return df
-
 col1, col2 = st.columns([11, 1])
 
 col1.title("Envio e tratamento de arquivos")
@@ -28,22 +13,11 @@ col2.image("./assets/images/logo.png")
 
 st.divider()
 
-menu_lateral = st.sidebar
+st.subheader("Enviar arquivo")
+file = st.file_uploader("Escolha seu arquivo", type="xlsx")
 
-with menu_lateral:
-    selected = st.selectbox("Escolha uma opção", ["Enviar arquivo", "Visualizar arquivo enviado"])
-
-if selected:
-    if selected == "Enviar arquivo":
-        st.subheader("Enviar arquivo")
-        receberArquivo(st.file_uploader("Escolha seu arquivo", type="xlsx"))
-       
-
-    if selected == "Visualizar arquivo enviado":
-        st.subheader("Visualizar arquivo enviado")
-        if file:
-            st.success('teste')
-        else:
-            st.error('teste')
+if file:      
+    df = pd.read_excel(file) 
+    st.dataframe(df)
 
 
